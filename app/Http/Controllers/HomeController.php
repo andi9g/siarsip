647f1e915f30bce60278e7d71d6ac6cace594743
andi9g <andirizly.bayuputra@gmail.com>
2024-03-10 10:51:52 +0700

    oke

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\arsipM;
+use App\Models\bagikanM;
 use App\Models\User;
+use Auth;
 class HomeController extends Controller
 {
     /**
@@ -24,11 +26,16 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $iduser = Auth::user()->iduser;
+        $arsipku = arsipM::where("iduser", $iduser)->count();
+        $bagikan = bagikanM::where("iduser", $iduser)->count();
         $arsip = arsipM::count();
         $user = User::count();
         return view('home', [
             "arsip" => $arsip,
             "user" => $user,
+            "arsipku" => $arsipku,
+            "bagikan" => $bagikan,
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,15 @@
 @section('judul', "welcome")
 
 @section('kembali')
+@if (Auth::user()->identitas->akses == "admin")
 <div class="row pb-10 mt-3">
-    <div class="col-xl-6 col-lg-6 col-md-6 ">
+    <div class="col-xl-3 col-lg-6 col-md-6 mt-3 ">
         <div class="card-box height-100-p widget-style3">
             <div class="d-flex flex-wrap">
                 <div class="widget-data">
                     <div class="weight-700 font-24 text-dark">{{ $arsip }}</div>
                     <div class="font-14 text-secondary weight-500">
-                        Jumlah Data Arsip
+                        Total Arsip
                     </div>
                 </div>
                 <div class="widget-icon">
@@ -23,7 +24,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xl-6 col-lg-6 col-md-6 ">
+    <div class="col-xl-3 col-lg-6 col-md-6 mt-3 ">
         <div class="card-box height-100-p widget-style3">
             <div class="d-flex flex-wrap">
                 <div class="widget-data">
@@ -41,7 +42,81 @@
         </div>
     </div>
 
+    <div class="col-xl-3 col-lg-6 col-md-6 mt-3 ">
+        <div class="card-box height-100-p widget-style3">
+            <div class="d-flex flex-wrap">
+                <div class="widget-data">
+                    <div class="weight-700 font-24 text-dark">{{ $arsipku }}</div>
+                    <div class="font-14 text-secondary weight-500">
+                        Arsipku
+                    </div>
+                </div>
+                <div class="widget-icon">
+                    <div class="icon" data-color="#00eccf" style="color: rgb(0, 236, 207);">
+                        <i class="icon-copy fa fa-file-archive-o"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-md-6 mt-3 ">
+        <div class="card-box height-100-p widget-style3">
+            <div class="d-flex flex-wrap">
+                <div class="widget-data">
+                    <div class="weight-700 font-24 text-dark">{{ $bagikan }}</div>
+                    <div class="font-14 text-secondary weight-500">
+                        Dibagikan Ke Saya
+                    </div>
+                </div>
+                <div class="widget-icon">
+                    <div class="icon" data-color="#ff5b5b" style="color: rgb(255, 91, 91);">
+                        <span class="icon-copy fa fa-share-alt"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
+
+@else
+<div class="row pb-10 mt-3">
+<div class="col-xl-6 col-lg-6 col-md-6 mt-3 ">
+    <div class="card-box height-100-p widget-style3">
+        <div class="d-flex flex-wrap">
+            <div class="widget-data">
+                <div class="weight-700 font-24 text-dark">{{ $arsipku }}</div>
+                <div class="font-14 text-secondary weight-500">
+                    Arsipku
+                </div>
+            </div>
+            <div class="widget-icon">
+                <div class="icon" data-color="#00eccf" style="color: rgb(0, 236, 207);">
+                    <i class="icon-copy fa fa-file-archive-o"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-xl-6 col-lg-6 col-md-6 mt-3 ">
+    <div class="card-box height-100-p widget-style3">
+        <div class="d-flex flex-wrap">
+            <div class="widget-data">
+                <div class="weight-700 font-24 text-dark">{{ $bagikan }}</div>
+                <div class="font-14 text-secondary weight-500">
+                    Dibagikan Ke Saya
+                </div>
+            </div>
+            <div class="widget-icon">
+                <div class="icon" data-color="#ff5b5b" style="color: rgb(255, 91, 91);">
+                    <span class="icon-copy fa fa-share-alt"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+@endif
 @endsection
 
 @section('content')
